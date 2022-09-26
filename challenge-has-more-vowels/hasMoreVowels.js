303b0363f7e10ba6aa154e053782e18c2410da14
Bushra Salama <bushra.salama@gmail.com>
2022-09-26 09:18:09 -0500

    revised

--- challenge-has-more-vowels/hasMoreVowels.js
+++ challenge-has-more-vowels/hasMoreVowels.js
@@ -1,13 +1,13 @@
-const hasMoreVowels(word){
-    let vowelnum = 0;
-    let consnum = 0;
-    for (let i = 0; i <word.length; i++){
-        if (word[i] == "a"|| word[i] == "e"|| word[i] == "o"||word[i]=="u"||word[i]=="i"){
-            vowelnum+=1
-        } else consnum+=1
-    }
-    if (vowelnum>consnum){
-        return true
-    } else return false
+function hasMoreVowels(word){
+    let vowelnum = 0; //initializes vowel count
+    let consnum = 0; // initializes consonant count 
+    for (let i = 0; i <word.length; i++)
+        if (word[i] == "a"|| word[i] == "e"|| word[i] == "o"||word[i]=="u"||word[i]=="i") 
+        //iterates through the string and checks against vowels with OR
+            vowelnum+=1;
+        else consnum+=1 // if the element is not a vowel, add to the consonant count;
+    if (vowelnum>consnum) // exit loop and check to see if vowel count is larger
+        return true;
+    else return false;
 }
-hasMoreVowels("Bushra");
+console.log(hasMoreVowels("aaaaoooooooh")); 
