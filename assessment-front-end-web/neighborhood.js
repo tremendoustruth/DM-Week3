a92dcdfb71671d0c2dd283eb5d59ec00c27a09c8
Bushra Salama <bushra.salama@gmail.com>
2022-10-02 18:57:38 -0500

    revised

--- assessment-front-end-web/neighborhood.js
+++ assessment-front-end-web/neighborhood.js
@@ -1,6 +1,6 @@
-let form = document.getElementById("restaurants");
+let button = document.getElementById("restaurants");
 let restauraunts = ["The Beehive", "Wink and Nod", "Picco"];
-form.addEventListener("click", restaurantMe(restauraunts));
+button.addEventListener("click", restaurantMe(restauraunts));
 function restaurantMe(arr){
     restaurant = arr[Math.floor(Math.random()*arr.length)]
     alert(`Try out ${restaurant}`);
